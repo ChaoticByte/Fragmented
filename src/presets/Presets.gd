e18fd7a14edd4b25a99a336ff644c88c2e100743
ChaoticByte <contact@chaoticbyte.net>
2024-06-19 14:50:43 +0200

    Added filter rgb_uv_distort.gdshader and improved greyscale.gdshader

--- src/presets/Presets.gd
+++ src/presets/Presets.gd
@@ -6,6 +6,7 @@ const dir = "res://src/presets/shaders/"
 	"Empty": load(dir + "empty.gdshader"),
 	"Greyscale": load(dir + "greyscale.gdshader"),
 	"Lowpass": load(dir + "lowpass.gdshader"),
+	"RGB -> UV Distort": load(dir + "rgb_uv_distort.gdshader"),
 	"Mix": load(dir + "mix.gdshader")
 }
 

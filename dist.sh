da1bd6a58a0d184cb0071a6841341e74aec74ee4
ChaoticByte <contact@chaoticbyte.net>
2025-02-04 18:59:57 +0100

    Don't include the shaderlib in the application anymore, instead distribute a zip file with each release

--- dist.sh
+++ dist.sh
@@ -10,13 +10,19 @@ VERSION="$(godot --headless --no-header -s tools/get_version.gd)"
 
 godot --headless --export-release "Linux/X11" "dist/Fragmented-${VERSION}.x86_64"
 
+log Packing shaderlib
+
+ZIP_PATH_SHADERLIB=$(realpath "dist/Fragmented-${VERSION}_shaderlib.zip")
+
+zip -r "${ZIP_PATH_SHADERLIB}" shaderlib/
+
 log Packing project template
 
-ZIP_PATH=$(realpath "dist/Fragmented-${VERSION}_project_template.zip")
+ZIP_PATH_PROJECT_TEMPLATE=$(realpath "dist/Fragmented-${VERSION}_project_template.zip")
 
-rm -f "${ZIP_PATH}"
-zip -r "${ZIP_PATH}" shaderlib/
+rm -f "${ZIP_PATH_PROJECT_TEMPLATE}"
 (
     cd examples/
-    zip -r "${ZIP_PATH}" *
+    mv project.godot_ project.godot && trap "mv project.godot project.godot_" EXIT
+    zip -r "${ZIP_PATH_PROJECT_TEMPLATE}" *
 )
